a6f755e6681dcf42f81b91be9308eab5a42ece79
Thitisan Shittisan <teesharkksv55@gmail.com>
2025-12-25 13:30:35 +0700

    Thitisan

---
+++ ENGCE117/Lab4/4.5.c
@@ -0,0 +1,63 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+struct studentNode {
+    char name[20];
+    int age;
+    char sex;
+    float gpa;
+    struct studentNode *next;
+};
+
+void SaveNode(struct studentNode *node, char name[], int age, char sex, float gpa);
+void GoNext2(struct studentNode **walk);
+
+int main() {
+    struct studentNode *start;
+    struct studentNode **now2;
+
+    start = (struct studentNode *)malloc(sizeof(struct studentNode));
+    SaveNode(start, "one", 6, 'M', 3.11);
+
+    start->next = (struct studentNode *)malloc(sizeof(struct studentNode));
+    SaveNode(start->next, "two", 8, 'F', 3.22);
+
+    start->next->next = (struct studentNode *)malloc(sizeof(struct studentNode));
+    SaveNode(start->next->next, "three", 10, 'M', 3.33);
+
+    start->next->next->next =
+        (struct studentNode *)malloc(sizeof(struct studentNode));
+    SaveNode(start->next->next->next, "four", 1ddfd2, 'F', 3.44);
+
+    now2 = &start;
+
+    GoNext2(now2);
+
+    printf("now2 points to: %s\n", (*now2)->name);
+
+    return 0;
+}
+
+void SaveNode(struct studentNode *node, char name[], int age, char sex, float gpa) {
+    strcpy(node->name, name);
+    node->age = age;
+    node->sex = sex;
+    node->gpa = gpa;
+    node->next = NULL;
+}
+
+void GoNext2(struct studentNode **walk) {
+    if (*walk != NULL) {
+        if ((*walk)->next != NULL) {
+            *walk = (*walk)->next;
+
+            printf("Name: %s\n", (*walk)->name);
+            printf("Age: %d\n", (*walk)->age);
+            printf("Sex: %c\n", (*walk)->sex);
+            printf("GPA: %.2f\n", (*walk)->gpa);
+        } else {
+            printf("No next node\n");
+        }
+    }
+}
