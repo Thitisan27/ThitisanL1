6e4eb7c82f07daaad2bb46fd99e6c79657f3afb1
Thitisan Shittisan <teesharkksv55@gmail.com>
2025-12-28 19:33:02 +0700

    Thitisan

---
+++ ENGCE117/Lab5/5.1.c
@@ -0,0 +1,112 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+struct studentNode {
+    char name[20];
+    int age;
+    char sex;
+    float gpa;
+    struct studentNode *next;
+};
+
+struct studentNode* AddNode(struct studentNode **start,
+                            char name[], int age, char sex, float gpa);
+void InsNode(struct studentNode *now,
+             char name[], int age, char sex, float gpa);
+void DelNode(struct studentNode *now);
+void ShowAll(struct studentNode *walk);
+
+int main() {
+
+    struct studentNode *start;
+    struct studentNode *now;
+
+    start = NULL;
+    now = NULL;
+
+    now = AddNode(&start, "one", 6, 'M', 3.11);
+    ShowAll(start);
+
+    now = AddNode(&start, "two", 8, 'F', 3.22);
+    ShowAll(start);
+
+    InsNode(now, "three", 10, 'M', 3.33);
+    ShowAll(start);
+
+    InsNode(now, "four", 12, 'F', 3.44);
+    ShowAll(start);
+
+    DelNode(now);
+    ShowAll(start);
+
+    return 0;
+}
+
+struct studentNode* AddNode(struct studentNode **start,
+                            char name[], int age, char sex, float gpa) {
+
+    struct studentNode *newNode;
+    struct studentNode *temp;
+
+    newNode = (struct studentNode *)malloc(sizeof(struct studentNode));
+
+    strcpy(newNode->name, name);
+    newNode->age = age;
+    newNode->sex = sex;
+    newNode->gpa = gpa;
+    newNode->next = NULL;
+
+    if (*start == NULL) {
+        *start = newNode;
+    } else {
+        temp = *start;
+        while (temp->next != NULL) {
+            temp = temp->next;
+        }
+        temp->next = newNode;
+    }
+
+    return newNode;
+}
+
+void InsNode(struct studentNode *now,
+             char name[], int age, char sex, float gpa) {
+
+    struct studentNode *newNode;
+
+    newNode = (struct studentNode *)malloc(sizeof(struct studentNode));
+
+    strcpy(newNode->name, name);
+    newNode->age = age;
+    newNode->sex = sex;
+    newNode->gpa = gpa;
+
+    newNode->next = now->next;
+    now->next = newNode;
+}
+
+void DelNode(struct studentNode *now) {
+
+    struct studentNode *temp;
+
+    if (now->next == NULL) {
+        return;
+    }
+
+    temp = now->next;
+    now->next = temp->next;
+    free(temp);
+}
+
+void ShowAll(struct studentNode *walk) {
+
+    printf("Linked List: ");
+
+    while (walk != NULL) {
+        printf("%s ", walk->name);
+        walk = walk->next;
+    }
+
+    printf("\n");
+}
