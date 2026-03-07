3c4407ff28f5c0fa8377f0d8c16f41f1a9c17ea3
Thitisan Shittisan <teesharkksv55@gmail.com>
2026-03-07 22:55:10 +0700

    thitisan

--- ENGCE117/Lab7/lab7.1.c
+++ ENGCE117/Lab7/lab7.1.c
@@ -1,96 +1,81 @@
-#include <iostream>
-#include <string.h>
 #include <stdio.h>
-
-using namespace std;
+#include <stdlib.h>
+#include <string.h>
 
 struct studentNode {
     char name[20];
     int age;
     char sex;
     float gpa;
-    studentNode *next;
+    struct studentNode *next;
 };
 
-class LinkedList {
-protected:
-    studentNode *start;   
-    studentNode **now;    
+void InsNode(struct studentNode **start, char n[], int a, char s, float g) {
+    struct studentNode *newNode = (struct studentNode*)malloc(sizeof(struct studentNode));
+    strcpy(newNode->name, n);
+    newNode->age = a;
+    newNode->sex = s;
+    newNode->gpa = g;
+    newNode->next = NULL;
 
-public:
-    LinkedList() {
-        start = NULL;
-        now = &start;
+    if (*start == NULL) {
+        *start = newNode;
+    } else {
+        struct studentNode *temp = *start;
+        while (temp->next != NULL) {
+            temp = temp->next;
+        }
+        temp->next = newNode;
     }
-    void InsNode(const char n[], int a, char s, float g) {
-        studentNode *newNode = new studentNode;
-        strcpy(newNode->name, n);
-        newNode->age = a;
-        newNode->sex = s;
-        newNode->gpa = g;
-        newNode->next = NULL;
+}
 
-        studentNode **temp = &start;
-        while (*temp != NULL) {
-            temp = &((*temp)->next);
-        }
-        *temp = newNode;
+void writefile(struct studentNode *start) {
+    FILE *fp = fopen("student.txt", "w");
+    if (fp == NULL) return;
+    
+    struct studentNode *temp = start;
+    while (temp != NULL) {
+        fprintf(fp, "%s %d %c %.2f\n", temp->name, temp->age, temp->sex, temp->gpa);
+        temp = temp->next;
     }
-    void GoNext() {
-        if (*now != NULL && (*now)->next != NULL) {
-            now = &((*now)->next);
-        }
-    }
-    void DelNode() {
-        if (*now != NULL) {
-            studentNode *temp = *now;
-            *now = (*now)->next;
-            delete temp;
-        }
-    }
-    virtual void ShowNode() {
-        if (*now != NULL) {
-            printf("%s %d %c %.2f\n", (*now)->name, (*now)->age, (*now)->sex, (*now)->gpa);
-        }
-    }
-};
+    fclose(fp);
+}
 
-class NewList : public LinkedList {
-public:
-    // ฟังก์ชันกลับไปเริ่มที่โหนดแรก
-    void GoFirst() {
-        now = &start;
+void readfile(struct studentNode **start) {
+    FILE *fp = fopen("student.txt", "r");
+    if (fp == NULL) return;
+
+    char n[20], s; int a; float g;
+    while (fscanf(fp, "%s %d %c %f", n, &a, &s, &g) != EOF) {
+        InsNode(start, n, a, s, g);
     }
-    virtual void ShowNode() {
-        if (*now != NULL) {
-            printf("%s %d %c %.2f\n", (*now)->name, (*now)->age, (*now)->sex, (*now)->gpa);
-        }
-    }
-};
+    fclose(fp);
+}
 
 int main() {
-    LinkedList listA;
-    NewList listB;
-    LinkedList *listC;
+    struct studentNode *listA = NULL;
+    int menu;
 
-    listA.InsNode("one", 1, 'A', 1.1);
-    listA.InsNode("two", 2, 'B', 2.2);
-    listA.InsNode("three", 3, 'C', 3.3);
-    listA.GoNext(); 
-    listA.ShowNode();
+    readfile(&listA);
 
-    listB.InsNode("four", 4, 'D', 4.4);
-    listB.InsNode("five", 5, 'E', 5.5);
-    listB.InsNode("six", 6, 'F', 6.6);
-    listB.GoNext();
-    listB.GoNext();
-    listB.ShowNode();
+    do {
+        printf("\n--- Menu (1)Add (5)ShowAll (0)Exit : ");
+        scanf("%d", &menu);
 
-    listC = &listA;
-    listC->ShowNode(); 
+        if (menu == 1) {
+            char n[20], s; int a; float g;
+            printf("Enter Data: ");
+            scanf("%s %d %c %f", n, &a, &s, &g);
+            InsNode(&listA, n, a, s, g);
+        } else if (menu == 5) {
+            struct studentNode *temp = listA;
+            while (temp != NULL) {
+                printf("%s %d %c %.2f\n", temp->name, temp->age, temp->sex, temp->gpa);
+                temp = temp->next;
+            }
+        }
+    } while (menu != 0);
 
-    listC = &listB;
-    listC->ShowNode(); 
-
+    writefile(listA);
     return 0;
 }
